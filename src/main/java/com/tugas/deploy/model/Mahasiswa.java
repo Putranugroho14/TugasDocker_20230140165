88bfb2cf5914b0fdbc2d6f78195b662d46833588
Putranugroho14 <putranugroho61@gmail.com>
2026-04-15 10:04:51 +0700

    Implementasi Model Data

---
+++ src/main/java/com/tugas/deploy/model/Mahasiswa.java
@@ -0,0 +1,13 @@
+package com.tugas.deploy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+}
